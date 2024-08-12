a71bfd501e084abf14774213e621c9cc4ba922ae
Hardik7843 <hardikprajapati7843@gmail.com>
2024-08-12 20:54:42 +0530

    milvus pipeline working

--- FAISS/ingestion_pipeline_milvus.py
+++ FAISS/ingestion_pipeline_milvus.py
@@ -1,4 +1,4 @@
-# from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core import VectorStoreIndex
 from sentence_transformers import SentenceTransformer
@@ -10,8 +10,6 @@ from llama_index.core.ingestion import (
     IngestionCache,
 )
 from llama_index.core.node_parser import SentenceSplitter
-
-from sentence_transformers import SentenceTransformer
 import logging
 import logging.config
 from llama_index.llms.groq import Groq
@@ -21,6 +19,13 @@ from llama_index.vector_stores.milvus import MilvusVectorStore
 from llama_index.core import VectorStoreIndex, StorageContext
 import os 
 from dotenv import load_dotenv
+from pymilvus import MilvusClient
+import pymilvus
+from llama_index.core import Settings
+
+
+# from llama_index import set_global_service_context
+
 
 logging.basicConfig(filename="built.log" , level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -43,16 +48,20 @@ def load_model():
     llm = Groq(model="llama3-70b-8192", api_key=GROQ_API_KEY)
     logger.info('LLM Loaded')
     
-
-    embed_model = SentenceTransformer("BAAI/bge-large-en-v1.5")
-    # embed_model = HuggingFaceEmbedding("BAAI/bge-large-en-v1.5")
+    embed_model = HuggingFaceEmbedding("BAAI/bge-large-en-v1.5")
     logger.info('Embedding Model Loaded')
 
     return llm , embed_model
 
+def create_milvus_vector_store(uri : str , dimention : int ) -> StorageContext:
+    vector_store = MilvusVectorStore(uri=uri, dim=dimention, overwrite=True)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    # logger.critical("created Vector Store , Storage Context from 'create_milvus_vector_store' ")
+    return vector_store , storage_context
 
-def build_pipeline(llm : Groq , embed_model : SentenceTransformer):
-
+def build_pipeline(llm : Groq , embed_model : HuggingFaceEmbedding , 
+                   vector_store : MilvusVectorStore ,
+                   storage_context : StorageContext):
     transformations = [
         SentenceSplitter(chunk_size=1024, chunk_overlap=20),
         TitleExtractor(
@@ -64,27 +73,26 @@ def build_pipeline(llm : Groq , embed_model : SentenceTransformer):
         embed_model
     ]
 
-    logger.critical('Built Transformations' , transformations)
-
-    vector_store = MilvusVectorStore(uri="./milvus_demo.db", dim=384, overwrite=True)
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    # logger.critical('Built Transformations' , transformations)
     
     
     documents = SimpleDirectoryReader("Data").load_data()
-    index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
-    docstore = SimpleDocumentStore()
-    logger.critical("Vector Store , Storage Context" , vector_store , storage_context)
 
-    pipeline = IngestionPipeline(transformations=transformations ,
+    index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, embed_model=embed_model)
+
+    docstore = SimpleDocumentStore()
+
+    pipeline = IngestionPipeline(transformations=transformations,
                             docstore=docstore,
                             vector_store=vector_store,
                             docstore_strategy=DocstoreStrategy.UPSERTS
                             )
     
-    logger.critical("Built the pipeline" , pipeline)
-    query_engine = index.as_query_engine()
+    logger.critical("Built the pipeline")
 
-    return query_engine , pipeline
+    query_engine = index.as_query_engine(llm = llm)
+
+    return query_engine, pipeline
 
     
 
@@ -93,5 +101,18 @@ def run_pipeline(documents : list ,  pipeline : IngestionPipeline):
     return nodes
 
 
+llm , embed_model = load_model()
+
+vector_store , storage_context = create_milvus_vector_store("milvus_demo.db" , 1024)
 
 
+
+query_engine , pipeline = build_pipeline(llm, embed_model, vector_store= vector_store, storage_context=storage_context)
+
+pipeline.persist("pipeline")
+# vector_store.persist(persist_dir = "persist")
+# storage_context.persist(persist_dir="persist")
+
+# res = query_engine.query("What is Agent?")
+
+# print(res)
